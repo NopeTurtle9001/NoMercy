3e7eb9b7c0c7eaf42b94379544a5b6e4fcce0c3e
NopeTurtle9001 <128744057+NopeTurtle9001@users.noreply.github.com>
2025-05-16 15:29:46 -0500

    Update SearchFunction.lua
    Fixed final bug

--- StarterGui/NewStarterGUI/SearchFunction.lua
+++ StarterGui/NewStarterGUI/SearchFunction.lua
@@ -10,25 +10,27 @@ local function onTextChanged()
 	--Get all player frames in plist scrolling frame
 	local PlayerList = script.Parent.Parent
 	for _, child in PlayerList:GetChildren() do
-		if child:IsA("Frame") and child.Name ~= "Header" and not (child:IsA("UIListLayout") or child:IsA("LocalScript") or child:IsA("TextBox")) then
+		if child:IsA("Frame") and not (child:IsA("UIListLayout") or child:IsA("LocalScript") or child:IsA("TextBox")) then
 			-- Get the ButtonForModeration 
 			local ButtonForMod = child:FindFirstChild("ButtonUsedForModeration")
 			if string.find(text, ButtonForMod.Text) then
+				print("Showing child named: "..child.Name)
 				child.Visible = true
 			else
+				print("Hiding child named: "..child.Name)
 				child.Visible = false
 			end
 		end
 	end
-	if text == " " or "" then
+	if text == " " or text == "" or text == "Enter Name Here" then
 		-- Handle the case where the text is empty
-		text = "Enter Name Here"
 		for _, child in PlayerList:GetChildren() do
 			-- Make all children visible
 			if child:IsA("UIListLayout") or child:IsA("LocalScript") or child:IsA("TextBox") then
 				-- Do nothing
 			else
 				child.Visible = true
+				print("Showing all children")
 			end
 		end
 	end
