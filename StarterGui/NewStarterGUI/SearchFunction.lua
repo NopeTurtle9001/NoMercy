0c4c6bc85b1825c373c39a9afcc1da53db1ed3c7
NopeTurtle9001 <128744057+NopeTurtle9001@users.noreply.github.com>
2025-05-16 12:41:40 -0500

    Create SearchFunction.lua

---
+++ StarterGui/NewStarterGUI/SearchFunction.lua
@@ -0,0 +1,31 @@
+--This Script will handle searching for players
+local Players = game:GetService("Players")
+
+local TextBox = script.Parent
+
+
+
+local function onTextChanged()
+	local text = TextBox.Text
+	--Get all player frames in plist scrolling frame
+	local PlayerList = script.Parent.Parent
+	for _, child in PlayerList:GetChildren() do
+		if child:IsA("Frame") and child.Name ~= "Header" then
+			if string.find(text, child.Name) then
+				child.Visible = true
+			else
+				child.Visible = false
+			end
+		end
+	end
+	if text == " " or "" then
+		-- Handle the case where the text is empty
+		text = "Enter Name Here"
+		for _, child in PlayerList:GetChildren() do
+			-- Make all children visible
+			child.Visible = true
+		end
+	end
+end
+
+TextBox:GetPropertyChangedSignal("Text"):Connect(onTextChanged)
